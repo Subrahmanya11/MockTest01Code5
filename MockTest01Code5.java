90d852e0c7b733814f3fb75f52cc505b020540b0
Subrahmanya11 <115634452+Subrahmanya11@users.noreply.github.com>
2023-07-03 08:35:41 +0530

    Add files via upload

---
+++ MockTest01Code5.java
@@ -0,0 +1,32 @@
+//Write a java program to declare and initialize variable of different 
+//data types (int,double,String)with appropriate identifiers 
+package in.ineuron.com;
+
+public class MockTest01Code5 {
+
+	public static void main(String[] args) {
+		//declaration & initialization
+		byte b=25;
+		char ch='s';
+		short s=10908;
+		int i =555000;
+		long l=8867516217L;
+		float f=2.54f;
+		double d= 2.54;
+		boolean bool=false;
+		String str="Subrahmanya";
+	
+		//printing the values
+		System.out.println("byte value :"+b);
+		System.out.println("char value :"+ch);
+		System.out.println("short value :"+s);
+		System.out.println("int value :"+i);
+		System.out.println("long value :"+l);
+		System.out.println("float value :"+f);
+		System.out.println("double value :"+d);
+		System.out.println("boolean value :"+bool);
+		System.out.println("String value :"+str);
+
+	}
+
+}
